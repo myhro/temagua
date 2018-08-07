a99ee23dc67054b6fd64e36183dec49950e291f2
Tiago Ilieve <tiago.myhro@gmail.com>
2018-08-07 11:34:01 +0200

    Add data for week 32

--- fetch.sh
+++ fetch.sh
@@ -3,6 +3,7 @@
 set -eu
 
 #CW30="http://www.copasa.com.br/wps/portal/internet/imprensa/noticias/plano-de-racionamento/em-racionamento/co-montes-claros/rodizio-em-montes-claros-23-07-2018/"
-URL="http://www.copasa.com.br/wps/portal/internet/imprensa/noticias/plano-de-racionamento/em-racionamento/co-montes-claros/rodizio-em-montes-claros-30-07/"
+#CW31="http://www.copasa.com.br/wps/portal/internet/imprensa/noticias/plano-de-racionamento/em-racionamento/co-montes-claros/rodizio-em-montes-claros-30-07/"
+URL="http://www.copasa.com.br/wps/portal/internet/imprensa/noticias/plano-de-racionamento/em-racionamento/co-montes-claros/rodizio-montes-claros-06-08-2018-a-14-08-2018-06-08-2018/"
 
 links -codepage utf-8 -dump "$URL" | grep -E '(Segunda|Terça|Quarta|Quinta|Sexta|Sábado|Domingo)'
