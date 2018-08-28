482beebe16547a6eff25fb8d87a3e144d885a25a
Tiago Ilieve <tiago.myhro@gmail.com>
2018-08-28 12:03:38 +0200

    Add data for week 35

--- fetch.sh
+++ fetch.sh
@@ -6,6 +6,7 @@ set -eu
 #CW31="http://www.copasa.com.br/wps/portal/internet/imprensa/noticias/plano-de-racionamento/em-racionamento/co-montes-claros/rodizio-em-montes-claros-30-07/"
 #CW32="http://www.copasa.com.br/wps/portal/internet/imprensa/noticias/plano-de-racionamento/em-racionamento/co-montes-claros/rodizio-montes-claros-06-08-2018-a-14-08-2018-06-08-2018/"
 #CW33="http://www.copasa.com.br/wps/portal/internet/imprensa/noticias/plano-de-racionamento/em-racionamento/co-montes-claros/rodizio-montes-claros-13-08-2018-a-21-08-2018-13-08-2018/"
-URL="http://www.copasa.com.br/wps/portal/internet/imprensa/noticias/plano-de-racionamento/em-racionamento/co-montes-claros/rodizio-em-montes-claros-20-08/"
+#CW34="http://www.copasa.com.br/wps/portal/internet/imprensa/noticias/plano-de-racionamento/em-racionamento/co-montes-claros/rodizio-em-montes-claros-20-08/"
+URL="http://www.copasa.com.br/wps/portal/internet/imprensa/noticias/plano-de-racionamento/em-racionamento/co-montes-claros/rodizio-em-montes-claros-27-08/"
 
 links -codepage utf-8 -dump "$URL" | grep -E '(Segunda|Terça|Quarta|Quinta|Sexta|Sábado|Domingo)'
