7c6f8fddc8d32490488722680df9b7402f8d77ee
Tiago Ilieve <tiago.myhro@gmail.com>
2019-12-30 11:39:49 -0300

    api: check if 'region' parameter is missing
    Also, using 'const' instead of 'let' helps a bit with type safety,
    forcing one to use different variables instead of overwritting a
    previous one with a different type.

--- api/src/interruption.js
+++ api/src/interruption.js
@@ -2,15 +2,21 @@ import { checkInterruption, checkOutdated } from './check';
 import response from './response';
 
 export async function getInterruption(url) {
-  let params = url.searchParams;
-  let region = parseInt(params.get('region'));
+  const res = {};
 
-  if (Number.isNaN(region) || region === null) {
-    return response({ error: 'not a valid region' }, 400);
+  const regionParam = url.searchParams.get('region');
+  if (regionParam === null || regionParam === '') {
+    res['error'] = 'missing region';
+    return response(res, 400);
   }
 
-  let outdated = await checkOutdated();
-  let res = {};
+  const region = parseInt(regionParam);
+  if (isNaN(region)) {
+    res['error'] = 'not a valid region';
+    return response(res, 400);
+  }
+
+  const outdated = await checkOutdated();
   if (outdated) {
     res['error'] = 'database is out of date';
     res['outdated'] = true;
